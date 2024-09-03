6034c7fead1b94f2a5e1e38bb5f397a62c52f984
RaytraceSG <azmi.maniku@gmail.com>
2024-09-03 10:15:28 +0800

    Update workflows and terraform

---
+++ sns_module.tf
@@ -0,0 +1,45 @@
+module "sns_topic" {
+  source  = "terraform-aws-modules/sns/aws"
+
+  name  = "ce7-azmi1-sns-tf-module"
+
+  topic_policy_statements = {
+    pub = {
+      actions = ["sns:Publish"]
+      principals = [{
+        type        = "AWS"
+        identifiers = ["arn:aws:iam::255945442255:role/publisher"]
+      }]
+    },
+
+    sub = {
+      actions = [
+        "sns:Subscribe",
+        "sns:Receive",
+      ]
+
+      principals = [{
+        type        = "AWS"
+        identifiers = ["*"]
+      }]
+
+      conditions = [{
+        test     = "StringLike"
+        variable = "sns:Endpoint"
+        values   = ["arn:aws:sqs:us-east-1:255945442255:subscriber"]
+      }]
+    }
+  }
+
+  subscriptions = {
+    sqs = {
+      protocol = "sqs"
+      endpoint = "arn:aws:sqs:us-east-1:255945442255:subscriber"
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
