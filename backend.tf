a4faa56bcf49b9a858b127459ce30416b7516bc0
RaytraceSG <azmi.maniku@gmail.com>
2024-09-03 11:17:00 +0800

    Ran terraform fmt

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@
 
 terraform {
   backend "s3" {
-    bucket = "sctp-ce7-tfstate" 
+    bucket = "sctp-ce7-tfstate"
     key    = "terraform-github-actions-azmi1.tfstate" #Change the value of this to <your suggested name>.tfstate for  example
     region = "us-east-1"
   }
