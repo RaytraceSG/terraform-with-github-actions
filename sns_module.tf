f9bdddcb65b674c948fed8053018297e8bd38ca6
RaytraceSG <azmi.maniku@gmail.com>
2024-09-03 10:27:13 +0800

    Update sns tf files

--- sns_module.tf
+++ sns_module.tf
@@ -1,45 +1,45 @@
-module "sns_topic" {
-  source  = "terraform-aws-modules/sns/aws"
+# module "sns_topic" {
+#   source  = "terraform-aws-modules/sns/aws"
 
-  name  = "ce7-azmi1-sns-tf-module"
+#   name  = "ce7-azmi1-sns-tf-module"
 
-  topic_policy_statements = {
-    pub = {
-      actions = ["sns:Publish"]
-      principals = [{
-        type        = "AWS"
-        identifiers = ["arn:aws:iam::255945442255:role/publisher"]
-      }]
-    },
+#   topic_policy_statements = {
+#     pub = {
+#       actions = ["sns:Publish"]
+#       principals = [{
+#         type        = "AWS"
+#         identifiers = ["arn:aws:iam::255945442255:role/publisher"]
+#       }]
+#     },
 
-    sub = {
-      actions = [
-        "sns:Subscribe",
-        "sns:Receive",
-      ]
+#     sub = {
+#       actions = [
+#         "sns:Subscribe",
+#         "sns:Receive",
+#       ]
 
-      principals = [{
-        type        = "AWS"
-        identifiers = ["*"]
-      }]
+#       principals = [{
+#         type        = "AWS"
+#         identifiers = ["*"]
+#       }]
 
-      conditions = [{
-        test     = "StringLike"
-        variable = "sns:Endpoint"
-        values   = ["arn:aws:sqs:us-east-1:255945442255:subscriber"]
-      }]
-    }
-  }
+#       conditions = [{
+#         test     = "StringLike"
+#         variable = "sns:Endpoint"
+#         values   = ["arn:aws:sqs:us-east-1:255945442255:subscriber"]
+#       }]
+#     }
+#   }
 
-  subscriptions = {
-    sqs = {
-      protocol = "sqs"
-      endpoint = "arn:aws:sqs:us-east-1:255945442255:subscriber"
-    }
-  }
+#   subscriptions = {
+#     sqs = {
+#       protocol = "sqs"
+#       endpoint = "arn:aws:sqs:us-east-1:255945442255:subscriber"
+#     }
+#   }
 
-  tags = {
-    Environment = "dev"
-    Terraform   = "true"
-  }
-}
+#   tags = {
+#     Environment = "dev"
+#     Terraform   = "true"
+#   }
+# }
